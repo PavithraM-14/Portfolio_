436a1e1d8c633a3a142fdd3c7d2fd3029e29aa01
PavithraM-14 <pavithramuralikrishnan1403@gmail.com>
2026-05-26 09:28:25 +0530

    Update repository URLs for new Portfolio_ repo

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [tailwindcss(), react()],
-  base: '/My_Portfolio_/',
+  base: '/Portfolio_/',
 })
